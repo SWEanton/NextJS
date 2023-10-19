7a242412512ee01126ba192efc6f67e558d2e9c5
SWEanton <Anton_9408@hotmail.com>
2023-10-19 23:20:34 +0200

    backgroundfix

--- nextjs-blog/pages/index.tsx
+++ nextjs-blog/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
 import Styles from '../styles/AirdropButton.module.css';
+import Image from 'next/image';
 
 const HomePage: React.FC = () => {
     const [clicked, setClicked] = useState(false);
@@ -10,20 +11,26 @@ const HomePage: React.FC = () => {
     }
 
     return (
-        <div>
-            <Header />
-            <main>
-                
-                <button 
-                    className={`${Styles.AirdropButton} ${clicked ? Styles.clicked : ''}`}
-                    onClick={handleButtonClick}
-                >
-                    Airdrop
-                </button>
-                <div className="features">
-                    {/* Feature icons and descriptions */}
-                </div>
-            </main>
+        <div style={{ position: 'relative', height: '100vh' }}>
+            {/* Background image */}
+            <Image 
+                src="/thumb-1920-947087.jpg"
+                alt="Background"
+                layout="fill"
+                objectFit="cover"
+            />
+            
+            {/* Content */}
+            <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 1 }}>
+                <Header />
+                <main>
+                    <button className={`${Styles.AirdropButton} ${clicked ? Styles.clicked : ''}`}
+                        onClick={handleButtonClick}>Airdrop</button>
+                    <div className="features">
+                        {/* Feature icons and descriptions */}
+                    </div>
+                </main>
+            </div>
         </div>
     );
 }
